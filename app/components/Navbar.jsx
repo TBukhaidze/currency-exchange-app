d37973eaf3a385571c287d655fadfb48480900a0
Temo Bukhaidze <bukhaidze.temo@gmail.com>
2025-02-08 23:37:04 +0400

    added main page

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -1,5 +1,36 @@
+import Image from "next/image";
+import search from "../../public/icons/search.svg";
+import diamond from "../../public/icons/diamond.svg";
+import user from "../../public/icons/user.svg";
+
 const Navbar = () => {
-  return;
+  return (
+    <div className="w-8/12 mx-auto my-7">
+      <div className="flex justify-between text-base md:text-3xl">
+        <div className="txt_orange">
+          <a href="/" className="flex">
+            ალმასი
+            <Image src={diamond} alt="diamond" />
+          </a>
+        </div>
+        <div>
+          <a href="">
+            <Image src={search} alt="search" className="mt-3" />
+          </a>
+        </div>
+        <div className="flex justify-center gap-2 personal_cab">
+          <Image src={user} alt="user" />
+          <a
+            href=""
+            className="txt_white nav_text "
+            style={{ fontSize: "0.933rem" }}
+          >
+            პირადი სივრცე
+          </a>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,21 @@
+import Main from "./components/Main";
+
 export default function Home() {
   return (
-    <main className="flex flex-col sm:flex-row h-screen">
-      <h1 className="meore">currency exchange app</h1>
+    <main className="main flex flex-col sm:flex-row ">
+      <div className="w-8/12 mx-auto pt-8">
+        <Main className="" />
+        <h1 className="meore">currency exchanges app</h1>
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+      </div>
     </main>
   );
 }
