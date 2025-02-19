297db0d7fe6f2caa5cbb13ddd2d14e7080fc9225
Temo Bukhaidze <bukhaidze.temo@gmail.com>
2025-02-19 22:34:43 +0400

    fixed a few minor bugs

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -2,31 +2,32 @@ import Image from "next/image";
 import search from "../../public/icons/search.svg";
 import diamond from "../../public/icons/diamond.svg";
 import user from "../../public/icons/user.svg";
+import Link from "next/link";
 
 const Navbar = () => {
   return (
     <div className="w-8/12 mx-auto my-7">
       <div className="flex justify-between text-base md:text-3xl">
         <div className="txt_orange">
-          <a href="/" className="flex">
+          <Link href="/" className="flex">
             ალმასი
             <Image src={diamond} alt="diamond" />
-          </a>
+          </Link>
         </div>
         <div>
-          <a href="">
+          <Link href="/" className="flex">
             <Image src={search} alt="search" className="mt-3" />
-          </a>
+          </Link>
         </div>
         <div className="flex justify-center gap-2 personal_cab">
           <Image src={user} alt="user" />
-          <a
-            href=""
+          <Link
+            href="/"
             className="txt_white nav_text "
             style={{ fontSize: "0.933rem" }}
           >
             პირადი სივრცე
-          </a>
+          </Link>
         </div>
       </div>
     </div>
--- app/components/Sidebar.jsx
+++ app/components/Sidebar.jsx
@@ -2,12 +2,17 @@ import Image from "next/image";
 
 import location from "../../public/icons/location.svg";
 import dolar from "../../public/icons/dolar.svg";
-import text from "../../public/icons/text.svg";
+import home from "../../public/icons/home.svg";
 import Link from "next/link";
 
 const Sidebar = () => {
   return (
     <div className="sidebar">
+      <div className="flex justify-center mb-4 sidebar_el">
+        <Link href="/">
+          <Image src={home} alt="home" />
+        </Link>
+      </div>
       <div className="flex justify-center mb-4 sidebar_el">
         <Link href="/">
           <Image src={dolar} alt="dolar" />
@@ -16,11 +21,6 @@ const Sidebar = () => {
       <div className="flex justify-center mb-4 sidebar_el">
         <Link href="/">
           <Image src={location} alt="location" />
-        </Link>
-      </div>
-      <div className="flex justify-center mb-4 sidebar_el">
-        <Link href="/">
-          <Image src={text} alt="text" />
         </Link>
       </div>
     </div>
