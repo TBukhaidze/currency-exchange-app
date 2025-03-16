7ce8f664d81e42bc27f04b246a42cca960fca872
Temo Bukhaidze <bukhaidze.temo@gmail.com>
2025-03-16 22:40:06 +0400

    rewrote fetch method to axios and change next.config

--- app/features/api/cryptoApi.js
+++ app/features/api/cryptoApi.js
@@ -1,15 +1,22 @@
+import axios from "axios";
+
 const API_URL = "https://api.coingecko.com/api/v3/coins/markets";
 
 export const fetchCryptoPrices = async () => {
   try {
-    const response = await fetch(
-      `${API_URL}?vs_currency=usd&order=market_cap_desc&per_page=20&page=1&sparkline=false`
-    );
-    if (!response.ok) throw new Error("Error data");
-    return await response.json();
+    const response = await axios.get(API_URL, {
+      params: {
+        vs_currency: "usd",
+        order: "market_cap_desc",
+        per_page: 20,
+        page: 1,
+        sparkline: false,
+      },
+    });
+
+    return response.data;
   } catch (error) {
     console.error("Error while receiving data:", error);
     return [];
   }
 };
-
