3476a388ef59d6a32924a139931201304a59b2f3
Temo Bukhaidze <bukhaidze.temo@gmail.com>
2025-03-16 23:23:52 +0400

    added styles to the grid and automatic percentage recognition

--- app/components/CryptoList.jsx
+++ app/components/CryptoList.jsx
@@ -24,54 +24,64 @@ export default function CryptoList() {
     return () => clearInterval(interval);
   }, []);
 
+  let num = 1;
+
   return (
-    <div className="w-8/12 mx-auto pt-8">
+    <div className="w-8/12 mx-auto pt-8 ">
       <div className="main_white main_exch">
-        <div className="flex justify-between text-center">
+        <div className="flex justify-between text-center pb-5">
           <div className="w-full grid grid-cols-5 gap-8">
             <div className="text-left col-span-2">
-              <h3 className="main_h3">ვალუტა</h3>
+              <h3 className="main_h3 pl-5">ვალუტა</h3>
             </div>
             <div>
               <h3 className="main_h3">ოფიციალური</h3>
             </div>
             <div>
-              <h3 className="main_h3">24h</h3>
+              <span className="font-light">24h</span>
             </div>
             <div>
               <h3 className="main_h3">კაპიტალიზაცია</h3>
             </div>
           </div>
         </div>
-        {coins.map((coin) => (
-          <div className="flex justify-between text-center" key={coin.id}>
-            <div key={coin.id} className="w-full grid grid-cols-5 gap-8">
-              <div className="text-left col-span-2">
-                <div className="flex gap-8  main_prices">
-                  <Image
-                    src={coin.image}
-                    alt={coin.name}
-                    width="20"
-                    height="20"
-                  />
-                  <p className="my-auto">
-                    {coin.name} ({coin.symbol.toUpperCase()}):
-                  </p>
+        {coins.map((coin) => {
+          const icon = coin.market_cap_change_percentage_24h < 0 ? down : up;
+
+          return (
+            <div key={coin.id}>
+              <div className="flex justify-between text-center my-3">
+                <div className="w-full grid grid-cols-5 gap-8">
+                  <div className="text-left col-span-2">
+                    <div className="flex gap-8 main_prices">
+                      {num++}
+                      <Image
+                        src={coin.image}
+                        alt={coin.name}
+                        width="24"
+                        height="24"
+                      />
+                      <p className="my-auto">
+                        {coin.name} ({coin.symbol.toUpperCase()})
+                      </p>
+                    </div>
+                  </div>
+                  <div className="flex justify-center my-auto main_prices">
+                    <span>{coin.current_price}</span>
+                  </div>
+                  <div className="my-auto main_prices flex justify-center">
+                    <span className="my-auto px-3">
+                      <Image src={icon} alt="change" />
+                    </span>
+                    {coin.market_cap_change_percentage_24h.toFixed(2)}%
+                  </div>
+                  <div className="my-auto main_prices">${coin.market_cap}</div>
                 </div>
               </div>
-              <div className="flex text-left my-auto main_prices">
-                <span>{coin.current_price}</span>
-              </div>
-              <div className="my-auto main_prices flex">
-                <span className="my-auto px-3">
-                  <Image src={up} alt="up" />
-                </span>
-                {coin.market_cap_change_percentage_24h}
-              </div>
-              <div className="my-auto main_prices">${coin.market_cap}</div>
+              <div className="p-4 border-t mx-8" />
             </div>
-          </div>
-        ))}
+          );
+        })}
       </div>
     </div>
   );
