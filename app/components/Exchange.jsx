ebd1a163f9986323b0ed0cd309ef7fff7bf47f38
Temo Bukhaidze <bukhaidze.temo@gmail.com>
2025-03-31 19:10:09 +0400

    added flags to array targetCurrencies and display of exchange component changed using map method

--- app/components/Exchange.jsx
+++ app/components/Exchange.jsx
@@ -1,5 +1,9 @@
 import Image from "next/image";
 
+import React, { useEffect, useState } from "react";
+
+import { fetchCurrencyPrice } from "../features/api/currencyApi";
+
 import up from "../../public/icons/up.svg";
 import down from "../../public/icons/down.svg";
 
@@ -15,8 +19,67 @@ import isr from "../../public/icons/isr.svg";
 import pol from "../../public/icons/pol.svg";
 
 const Exchange = () => {
+  const [date, setDate] = useState("");
+  const [data, setData] = useState(null);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  const flags = [usa, eur, rus, tr, uk, che, azr, arm, isr, pol];
+
+  useEffect(() => {
+    const today = new Date().toISOString().split("T")[0];
+    setDate(today);
+  }, []);
+
+  useEffect(() => {
+    const fetchRates = async () => {
+      setLoading(true);
+      setError(null);
+
+      try {
+        const result = await fetchCurrencyPrice(date);
+        console.log(result);
+        setData(result);
+      } catch (err) {
+        setError(err.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchRates();
+  }, [date]);
+
+  const targetCurrencies = [
+    "USD",
+    "EUR",
+    "RUB",
+    "TRY",
+    "GBP",
+    "CHF",
+    "AZN",
+    "AMD",
+    "ILS",
+    "PLN",
+  ];
+
+  const getFilteredCurrencies = (currencies) => {
+    return currencies
+      .filter((currency) => targetCurrencies.includes(currency.code))
+      .sort(
+        (a, b) =>
+          targetCurrencies.indexOf(a.code) - targetCurrencies.indexOf(b.code)
+      );
+  };
+
   return (
     <>
+      {loading && (
+        <div className="h-60 flex justify-center">
+          <div className="loader" />
+        </div>
+      )}
+      {error && <p className="text-red-500 mt-2">Error: {error}</p>}
       <div className="text-left col-span-2">
         <h3 className="main_h3">ვალუტა</h3>
       </div>
@@ -29,155 +92,32 @@ const Exchange = () => {
       <div>
         <h3 className="main_h3">გაყიდვა</h3>
       </div>
-      <div className="text-left col-span-2">
-        <div className="flex gap-8  main_prices">
-          <Image src={usa} alt="usaIcon" />
-          <p className="my-auto">1 დოლარი</p>
-        </div>
-      </div>
-      <div className="flex text-left my-auto main_prices">
-        <span>2.7940</span>
-        <span className="my-auto px-3">
-          <Image src={up} alt="up" />
-        </span>
-      </div>
-      <div className="my-auto main_prices">2.7750</div>
-      <div className="my-auto main_prices">2.7750 </div>
-
-      <div className="text-left col-span-2 ">
-        <div className="flex gap-8  main_prices">
-          <Image src={eur} alt="eurIcon" />
-          <p className="my-auto">1 ევრო</p>
-        </div>
-      </div>
-      <div className="flex text-left my-auto main_prices">
-        <span>2.9010</span>
-        <span className="my-auto px-3">
-          <Image src={down} alt="up" />
-        </span>
-      </div>
-      <div className="my-auto main_prices">2.8650</div>
-      <div className="my-auto main_prices">2.8800 </div>
-
-      <div className="text-left col-span-2 ">
-        <div className="flex gap-8  main_prices">
-          <Image src={rus} alt="rusIcon" />
-          <p className="my-auto">100 რუბლი</p>
-        </div>
-      </div>
-      <div className="flex text-left my-auto main_prices">
-        <span>2.8840</span>
-        <span className="my-auto px-3">
-          <Image src={down} alt="up" />
-        </span>
-      </div>
-      <div className="my-auto main_prices">2.7500</div>
-      <div className="my-auto main_prices">2.9000 </div>
-
-      <div className="text-left col-span-2 ">
-        <div className="flex gap-8  main_prices">
-          <Image src={tr} alt="turIcon" />
-          <p className="my-auto">1 ლირა</p>
-        </div>
-      </div>
-      <div className="flex text-left my-auto main_prices">
-        <span>0.0776</span>
-        <span className="my-auto px-3">
-          <Image src={up} alt="up" />
-        </span>
-      </div>
-      <div className="my-auto main_prices">0.0765</div>
-      <div className="my-auto main_prices">0.0835 </div>
-
-      <div className="text-left col-span-2 ">
-        <div className="flex gap-8  main_prices">
-          <Image src={uk} alt="ukIcon" />
-          <p className="my-auto">1 ფუნტი</p>
-        </div>
-      </div>
-      <div className="flex text-left my-auto main_prices">
-        <span>3.4794</span>
-        <span className="my-auto px-3">
-          <Image src={up} alt="up" />
-        </span>
-      </div>
-      <div className="my-auto main_prices">3.4280</div>
-      <div className="my-auto main_prices">3.4710 </div>
-
-      <div className="text-left col-span-2 ">
-        <div className="flex gap-8  main_prices">
-          <Image src={che} alt="cheIcon" />
-          <p className="my-auto">1 ფრანკი</p>
-        </div>
-      </div>
-      <div className="flex text-left my-auto main_prices">
-        <span>3.0801</span>
-        <span className="my-auto px-3">
-          <Image src={up} alt="up" />
-        </span>
-      </div>
-      <div className="my-auto main_prices">3.0400</div>
-      <div className="my-auto main_prices">3.0850 </div>
-
-      <div className="text-left col-span-2 ">
-        <div className="flex gap-8  main_prices">
-          <Image src={azr} alt="azrIcon" />
-          <p className="my-auto">1 მანათი</p>
-        </div>
-      </div>
-      <div className="flex text-left my-auto main_prices">
-        <span>1.6431</span>
-        <span className="my-auto px-3">
-          <Image src={down} alt="up" />
-        </span>
-      </div>
-      <div className="my-auto main_prices">1.5255</div>
-      <div className="my-auto main_prices">1.6570 </div>
-
-      <div className="text-left col-span-2 ">
-        <div className="flex gap-8  main_prices">
-          <Image src={arm} alt="armIcon" />
-          <p className="my-auto">1000 დრამი</p>
-        </div>
-      </div>
-      <div className="flex text-left my-auto main_prices">
-        <span>7.0245</span>
-        <span className="my-auto px-3">
-          <Image src={down} alt="up" />
-        </span>
-      </div>
-      <div className="my-auto main_prices">5.8175</div>
-      <div className="my-auto main_prices">7.3080</div>
-
-      <div className="text-left col-span-2 ">
-        <div className="flex gap-8  main_prices">
-          <Image src={isr} alt="isrIcon" />
-          <p className="my-auto">10 შეკელი</p>
-        </div>
-      </div>
-      <div className="flex text-left my-auto main_prices">
-        <span>7.8605</span>
-        <span className="my-auto px-3">
-          <Image src={up} alt="up" />
-        </span>
-      </div>
-      <div className="my-auto main_prices">6.2135</div>
-      <div className="my-auto main_prices">8.2385</div>
-
-      <div className="text-left col-span-2 ">
-        <div className="flex gap-8  main_prices">
-          <Image src={pol} alt="polIcon" />
-          <p className="my-auto">10 ზლოტი</p>
-        </div>
-      </div>
-      <div className="flex text-left my-auto main_prices">
-        <span>6.9205</span>
-        <span className="my-auto px-3">
-          <Image src={up} alt="up" />
-        </span>
-      </div>
-      <div className="my-auto main_prices">5.7370</div>
-      <div className="my-auto main_prices">7.0835</div>
+      {data && (
+        <>
+          {getFilteredCurrencies(data[0]?.currencies || []).map(
+            (currency, index) => (
+              <React.Fragment key={currency.code}>
+                <div key={currency.code} className="text-left col-span-2">
+                  <div className="flex gap-8 main_prices">
+                    <Image src={flags[index]} alt="usaIcon" />
+                    <p className="my-auto">
+                      {currency.quentity} {currency.name}
+                    </p>
+                  </div>
+                </div>
+                <div className="flex text-left my-auto main_prices">
+                  <span>{currency.rate}</span>
+                  <span className="my-auto px-3">
+                    <Image src={up} alt="up" />
+                  </span>
+                </div>
+                <div className="my-auto main_prices">{currency.rate}</div>
+                <div className="my-auto main_prices">{currency.rate}</div>
+              </React.Fragment>
+            )
+          )}
+        </>
+      )}
     </>
   );
 };
