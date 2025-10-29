9e8ebf85d4b3b3e3c11edfe474d00c7a9aec1c58
Temo Bukhaidze <bukhaidze.temo@gmail.com>
2025-10-29 19:58:04 +0400

    refactor: migrate getCurrentTime to TypeScript

--- app/utils/getCurrentTime.ts
+++ app/utils/getCurrentTime.ts
@@ -1,4 +1,4 @@
-export const getCurrentTime = () => {
+export const getCurrentTime = (): string => {
   const date = new Date();
   const hours = String(date.getHours()).padStart(2, "0");
   const minutes = String(date.getMinutes()).padStart(2, "0");
